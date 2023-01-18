7841e728304a4bd17bd014ffdea1e5069cdd6c74
Israfil Tagulao <israfil.tagulao@cvsu.edu.ph>
2023-01-18 23:20:57 +0800

    change footer text

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -74,7 +74,7 @@
         <router-view class="container" />
         <footer class="footer mt-auto py-3 bg-light">
             <div class="container">
-                <span class="text-muted">Place sticky footer content here.</span>
+                <span class="text-muted">NTFH.</span>
             </div>
         </footer>
     </div>
